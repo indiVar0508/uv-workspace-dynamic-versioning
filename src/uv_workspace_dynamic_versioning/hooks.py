d208aed1daf9aa6715f8fc9fc1e104cb91f75a42
Indivar Mishra <indimishra@gmail.com>
2026-03-17 03:19:24 +0530

    Initial commit for uv-workspace-dynamic-versioning

---
+++ src/uv_workspace_dynamic_versioning/hooks.py
@@ -0,0 +1,11 @@
+from hatchling.plugin import hookimpl
+from .plugin import DynamicWorkspaceVersionSource
+from .vendored.metadata_hook import DependenciesMetadataHook
+
+@hookimpl
+def hatch_register_version_source():
+    return DynamicWorkspaceVersionSource
+
+@hookimpl
+def hatch_register_metadata_hook():
+    return DependenciesMetadataHook
---
+++ src/uv_workspace_dynamic_versioning/plugin.py
@@ -0,0 +1,3 @@
+from .vendored.version_source import DynamicWorkspaceVersionSource
+
+__all__ = ["DynamicWorkspaceVersionSource"]
